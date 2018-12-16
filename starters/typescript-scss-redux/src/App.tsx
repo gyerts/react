34fc5b814cdc87f175cbed0476d3d023ac070ef2
Yuriy Gyerts <yuriy.gyerts@syntech.software>
2018-12-16 16:36:26 +0200

    add env files

--- starters/typescript-scss-redux/src/App.tsx
+++ starters/typescript-scss-redux/src/App.tsx
@@ -3,29 +3,32 @@ import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
 
-
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-           <Link to="/about">About the author</Link>
-        </header>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+               <p>
+                  Edit <code>src/App.js</code> and save to reload.
+               </p>
+               <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  Learn React
+               </a>
+               <Link to="/about">About the author</Link>
+               <ul>
+                  <li>{process.env.REACT_APP_ENV_FILE}</li>
+                  <li>{process.env.REACT_APP_SECRET}</li>
+               </ul>
+            </header>
+         </div>
+      );
+   }
 }
 
 export default App;
